e1cce9e911d426f46dd63172f642f0c3709dc12a
Mustafa Cantürk Karabulut <karabulut_3828@hotmail.com>
2024-02-01 16:37:07 +0300

    Medium Question & Subregion
    Medium soruları tamamlandı.Subregion ve Home Screen için ortak bir data class kullanrak referans verdim.Çünkü aynı itemleri parametre alıyor.Gereksiz kod kullanımından kaçmaya çalıştım.

--- app/src/main/java/com/example/countriesapp/presentation/region_subregion/subregion/SubregionScreen.kt
+++ app/src/main/java/com/example/countriesapp/presentation/region_subregion/subregion/SubregionScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -26,12 +27,32 @@ import com.example.countriesapp.common.Constants.Southern_Asia
 import com.example.countriesapp.common.Constants.Western_Africa
 import com.example.countriesapp.layouts.AppBar
 import com.example.countriesapp.layouts.HomeCard
+import com.example.countriesapp.presentation.ui_model.ScreenItem
 
 @Composable
 fun SubRegionScreen(
     backClick: (() -> Unit)? = null,
     clickSubRegionItem: ((String) -> Unit)? = null
 ) {
+    val subRegionItem = listOf(
+        ScreenItem(cardText = "Southern Europe", clickHomeItem = { clickSubRegionItem?.invoke(SOUTHERN_EUROPE) }, backgroundColor = Color.LightGray),
+        ScreenItem(cardText = "South-Eastern Asia", clickHomeItem = { clickSubRegionItem?.invoke(SOUTH_EASTERN_ASIA) }, backgroundColor = Color.Blue),
+        ScreenItem(cardText = "North America", clickHomeItem = { clickSubRegionItem?.invoke(North_America) }, backgroundColor = Color.Cyan),
+        ScreenItem(cardText = "Melanesia", clickHomeItem = { clickSubRegionItem?.invoke(Melanesia) }, backgroundColor = Color.Yellow),
+        ScreenItem(cardText = "Central Europe", clickHomeItem = { clickSubRegionItem?.invoke(Central_Europe) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Eastern Africa", clickHomeItem = { clickSubRegionItem?.invoke(Eastern_Africa) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Western Africa", clickHomeItem = { clickSubRegionItem?.invoke(Western_Africa) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Northern Africa", clickHomeItem = { clickSubRegionItem?.invoke(Northern_Africa) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Southern Africa", clickHomeItem = { clickSubRegionItem?.invoke(Southern_Africa) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Northern Europe", clickHomeItem = { clickSubRegionItem?.invoke(Northern_Europe) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Caribbean", clickHomeItem = { clickSubRegionItem?.invoke(Caribbean) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "South America", clickHomeItem = { clickSubRegionItem?.invoke(South_America) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Southeast Europe", clickHomeItem = { clickSubRegionItem?.invoke(Southeast_Europe) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Middle Africa", clickHomeItem = { clickSubRegionItem?.invoke(Middle_Africa) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Southern Asia", clickHomeItem = { clickSubRegionItem?.invoke(Southern_Asia) }, backgroundColor = Color.DarkGray),
+        ScreenItem(cardText = "Eastern Asia", clickHomeItem = { clickSubRegionItem?.invoke(Eastern_Asia) }, backgroundColor = Color.DarkGray)
+    )
+
     Column {
         AppBar(backButtonCheck = true,
             imageId = R.drawable.icons_turkey,
@@ -42,145 +63,12 @@ fun SubRegionScreen(
         LazyVerticalStaggeredGrid(
             columns = StaggeredGridCells.Fixed(2),
             contentPadding = PaddingValues(10.dp)
-        ) {
-            item {
-                HomeCard(cardText = "Southern Europe",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(SOUTHERN_EUROPE)
-                    }, backgroundColor = Color.LightGray
-                )
-            }
-            item {
-                HomeCard(cardText = "South-Eastern Asia",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(SOUTH_EASTERN_ASIA)
-                    }, backgroundColor = Color.Blue
-                )
-            }
-            item {
+        ){
+            items(subRegionItem) { subRegionItem ->
                 HomeCard(
-                    cardText = "North America",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(North_America)
-                    },
-                    backgroundColor = Color.Cyan
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Melanesia",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Melanesia)
-                    },
-                    backgroundColor = Color.Yellow
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Central Europe",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Central_Europe)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Eastern Africa",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Eastern_Africa)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Western Africa",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Western_Africa)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Northern Africa",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Northern_Africa)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Southern Africa",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Southern_Africa)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Northern Europe",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Northern_Europe)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Caribbean",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Caribbean)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "South America",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(South_America)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Southeast Europe",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Southeast_Europe)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Middle Africa",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Middle_Africa)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Southern Asia",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Southern_Asia)
-                    },
-                    backgroundColor = Color.DarkGray
-                )
-            }
-            item {
-                HomeCard(
-                    cardText = "Eastern Asia",
-                    clickHomeCardItem = {
-                        clickSubRegionItem?.invoke(Eastern_Asia)
-                    },
-                    backgroundColor = Color.DarkGray
+                    cardText = subRegionItem.cardText,
+                    clickHomeCardItem = { subRegionItem.clickHomeItem.invoke(it) },
+                    backgroundColor = subRegionItem.backgroundColor
                 )
             }
         }
